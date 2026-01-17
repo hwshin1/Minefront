1305ebbe858619ba6e1e7c18fc90d8a33fa43e03
hwshin1 <hwshin5044@gmail.com>
2026-01-17 23:56:18 +0900

    버퍼전략 초기화 및 pixels 생성

--- src/com/mine/minefront/Display.java
+++ src/com/mine/minefront/Display.java
@@ -1,9 +1,13 @@
 package com.mine.minefront;
 
 import com.mine.minefront.graphics.Render;
+import com.mine.minefront.graphics.Screen;
 
 import javax.swing.JFrame;
-import java.awt.Canvas;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
 
 // 디스플레이
 public class Display extends Canvas implements Runnable {
@@ -17,10 +21,16 @@ public class Display extends Canvas implements Runnable {
     private Thread thread; // 스레드 객체
     private boolean running = false;
     private Render render;
+    private Screen screen;
+    private BufferedImage img;
+    private int[] pixels; // 별도의 픽셀 배열변수
 
-    // Render 픽셀 설정
     public Display() {
-        render = new Render(WIDTH, HEIGHT);
+        // render = new Render(WIDTH, HEIGHT);
+        // render 없애고 screen 으로 변경
+        screen = new Screen(WIDTH, HEIGHT);
+        img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        pixels = ((DataBufferInt)img.getRaster().getDataBuffer()).getData();
     }
 
     // 스레드 초기화
@@ -58,7 +68,28 @@ public class Display extends Canvas implements Runnable {
     private void tick() {}
 
     // 렌더링
-    private void render() {}
+    private void render() {
+        // 버퍼전략 초기화
+        BufferStrategy bs = this.getBufferStrategy();
+
+        // 버퍼 생성
+        if (bs == null) {
+            // 2차원은 2개 3차원은 3개 / 3D
+            createBufferStrategy(3);
+            return;
+        }
+
+        screen.render();
+
+        for (int i = 0; i < WIDTH * HEIGHT; i++) {
+            pixels[i] = screen.pixels[i];
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.drawImage(img, 0, 0, WIDTH, HEIGHT, null);
+        g.dispose();
+        bs.show();
+    }
 
     public static void main(String[] args) {
         Display game = new Display();
--- src/com/mine/minefront/graphics/Screen.java
+++ src/com/mine/minefront/graphics/Screen.java
@@ -1,11 +1,22 @@
 package com.mine.minefront.graphics;
 
+import java.util.Random;
+
 public class Screen extends Render {
 
     private Render test;
 
     public Screen(int width, int height) {
         super(width, height);
+
+        // Random 함수
+        Random random = new Random();
+
+        // 작업을 256번 반복 / 너비,높이 = 256
+        test = new Render(256, 256);
+        for (int i = 0; i < 256 * 256; i++) {
+            test.pixels[i] = random.nextInt();
+        }
     }
 
     public void render() {
