03a0bef909d61caf409de228bb87b32c8ac1460c
hwshin1 <hwshin5044@gmail.com>
2026-01-26 11:40:57 +0900

    xx, yy변수 타입 double로 변경

--- src/com/mine/minefront/graphics/Render3D.java
+++ src/com/mine/minefront/graphics/Render3D.java
@@ -13,18 +13,29 @@ public class Render3D extends Render{
         // y축 높이를 위한 전체 루프
         for (int y = 0; y < height; y++){
             double ceiling = (y - height / 2.0) / height;
-            double z = 2.0 / ceiling;
 
-            time+=0.00005; // time 조절
+            // 천장값을 반전시키기
+            if (ceiling < 0) {
+                ceiling = -ceiling;
+            }
+
+            double z = 8.0 / ceiling;
+
+            time += 0.0001; // time 조절
 
             for (int x = 0; x < width; x++) {
                 double depth = (x - width / 2.0) / height;
                 depth *= z;
 
                 // 비트 연산자로 설정
-                int xx = (int) (depth) & 15;
-                int yy = (int) (z + time) & 15; // time으로 애니메이션 처럼 보이게
-                pixels[x + y * width] = (xx * 16) | (yy * 16) << 8;
+                // time으로 애니메이션 처럼 보이게
+                double xx = depth; // time 추가시 행 이동
+                double yy = z + time; // time 추가시 열 이동
+
+                // xx,yy를 double 타입으로 저장하고 싶어서 만든 변수들
+                int xPix = (int) (xx);
+                int yPix = (int) (yy);
+                pixels[x + y * width] = ((xPix & 15) * 16) | ((yPix & 15) * 16) << 8;
 //                System.out.println(xx); // 진단용 프린트
             }
         }
